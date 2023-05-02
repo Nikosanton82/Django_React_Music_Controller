8b1ed0d02348fade498ee4131cd2fa8daf8ef6ac
Nikolaos Antonakakis <nikosanton82@gmail.com>
2023-05-03 00:34:36 +0200

    Few fixes

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
 import HomePage from "./HomePage";
-
 
 export default class App extends Component {
     constructor(props) {
@@ -9,12 +7,10 @@ export default class App extends Component {
     }
 
     render() {
-        return (<div>
-        <HomePage />
-        </div>
+        return (
+            <div>
+                <HomePage />
+            </div>
         );
     }
 }
-
-const appDiv = document.getElementById("app");
-render(<App />, appDiv);
