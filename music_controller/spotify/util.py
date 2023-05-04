04f3c6525d4f0ff0855c3f39234b0c5bba724582
Nikolaos Antonakakis <nikosanton82@gmail.com>
2023-05-04 09:20:59 +0200

    Pausing & Playing Music With Spotify API #15

--- music_controller/spotify/util.py
+++ music_controller/spotify/util.py
@@ -58,7 +58,7 @@ def refresh_spotify_token(session_id):
     access_token = response.get('access_token')
     token_type = response.get('token_type')
     expires_in = response.get('expires_in')
-    refresh_token = response.get('refresh_token')
+    # refresh_token = response.get('refresh_token')
 
     update_or_create_user_tokens(session_id, access_token, token_type, expires_in, refresh_token)
 
@@ -78,3 +78,9 @@ def execute_spotify_api_request(session_id, endpoint, post_=False, put_=False):
         print("Error status code:", response.status_code)  # Add this line
         print("Error response text:", response.text)  # Add this line
         return {'Error': 'Issue with request'}
+    
+def play_song(session_id):
+    return execute_spotify_api_request(session_id, "player/play", put_=True)
+
+def pause_song(session_id):
+    return execute_spotify_api_request(session_id, "player/pause", put_=True)
